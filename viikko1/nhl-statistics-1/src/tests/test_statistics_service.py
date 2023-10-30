671d2eb85ec384fee4c58f4da19b29e71aa264ff
Kalle Hahl <kalle.hahl@hotmail.com>
2023-10-30 21:07:03 +0200

    tehtävät 15-17

---
+++ viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
@@ -0,0 +1,55 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+from enum import Enum
+
+
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
+
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        self.stats = StatisticsService(PlayerReaderStub())
+
+    def test_search_works(self):
+        self.assertEqual('Semenko', self.stats.search('Semenko').name)
+        self.assertIsNone(self.stats.search('Jalmari'))
+
+    def test_team_method_works(self):
+        players = self.stats.team("PIT")
+        self.assertEqual(1, len(players))
+        self.assertEqual('Lemieux', players[0].name)
+
+    def test_top_method_works(self):
+        top_players = self.stats.top(3)
+        self.assertEqual(4, len(top_players))
+
+    def test_top_method_works_goals(self):
+        top_players = self.stats.top(1, SortBy.GOALS)
+        self.assertEqual('Lemieux', top_players[0].name)
+        self.assertEqual("Yzerman", top_players[1].name)
+
+    def test_top_method_works_assists(self):
+        top_players = self.stats.top(1, SortBy.ASSISTS)
+        self.assertEqual('Gretzky', top_players[0].name)
+        self.assertEqual("Yzerman", top_players[1].name)
+
+    def test_top_method_works_points(self):
+        top_players = self.stats.top(1, SortBy.POINTS)
+        self.assertEqual('Gretzky', top_players[0].name)
+        self.assertEqual("Lemieux", top_players[1].name)
